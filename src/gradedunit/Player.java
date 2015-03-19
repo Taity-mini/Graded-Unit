0009cdb0e4e58948863566a076397209951747be
Taity-mini <ec1302292@edinburghcollege.ac.uk>
2015-03-19 23:19:06 +0000

    Started Project Implementation. Created Consts, Files, Member and Player Classes.

---
+++ src/gradedunit/Player.java
@@ -0,0 +1,33 @@
+package gradedunit;
+
+
+/**
+ *
+ * @author Andrew Tait (EC1302292)
+ */
+public class Player extends Member
+{
+   //Getters 
+    
+
+    public String getName()
+      {
+        return Name;
+      }
+    
+
+    public void setName(String Name)
+      {
+        this.Name = Name;
+      }
+
+    public String getEmail()
+      {
+        return Email;
+      }
+
+   
+
+    
+    
+}
